02ff5b7b81a0152942827581666c554565fdf723
MuhammadUzair <Uzairsuleman786@gmail.com>
2021-10-16 17:26:19 +0500

    Perform CRUD Operation Using Ajax Along with just Single View in MVC.

---
+++ CrudUsingAjax/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using CrudUsingAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudUsingAjax.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDB edb = new EmployeeDB(); 
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            var listOfEmployee = edb.ListAll();
+            return Json(listOfEmployee, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Add(Employee emp)
+        {
+            var addEmployee = edb.Add(emp);
+            return Json(addEmployee,JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetById(int id)
+        {
+            var getById = edb.ListAll().Find(x => x.EmployeeID.Equals(id));
+            return Json(getById, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(Employee emp)
+        {
+            var updateEmployee = edb.Update(emp);
+            return Json(updateEmployee, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var deleteEmployee = edb.Delete(id);
+            return Json(deleteEmployee, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CrudUsingAjax/Models/EmployeeDB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CrudUsingAjax.Models
+{
+    public class EmployeeDB
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+        public List<Employee> ListAll()
+        {
+            List<Employee> listOfEmployee = new List<Employee>();
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("SelectEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    listOfEmployee.Add(
+                        new Employee
+                        {
+                            EmployeeID = Convert.ToInt32(sdr["EmployeeId"]),
+                            Name = sdr["Name"].ToString(),
+                            Age = Convert.ToInt32(sdr["Age"])
+                        });
+                }
+
+
+                con.Close();
+            }
+
+                return listOfEmployee;
+        }
+
+        public int Add(Employee emp)
+        {
+            int i;
+
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("InsertUpdateEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", emp.EmployeeID);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Age", emp.Age);
+                cmd.Parameters.AddWithValue("@Action", "Insert");
+                i = cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            return i;
+        }
+
+        public int Update(Employee emp)
+        {
+            int i;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("InsertUpdateEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", emp.EmployeeID);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Age", emp.Age);
+                cmd.Parameters.AddWithValue("@Action", "Update");
+                i = cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            return i;
+        }
+
+        public int Delete(int Id)
+        {
+            int i;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DeleteEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", Id);
+                i = cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            return i;
+        }
+    }
+}
